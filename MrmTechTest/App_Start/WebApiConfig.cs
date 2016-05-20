068ba5878e93d0660bc3fbd4fdc384d62adb9115
Nero Sule <sulenero@gmail.com>
2016-05-20 02:39:22 +0100

    Forgets file

--- MrmTechTest/App_Start/WebApiConfig.cs
+++ MrmTechTest/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Newtonsoft.Json;
 
 namespace MrmTechTest
@@ -10,21 +7,11 @@ namespace MrmTechTest
     {
         public static void Register(HttpConfiguration config)
         {
-            //var xml = GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(
-            //    x => x.MediaType == "application/xml");
-            //GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Remove(xml);
-            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-            
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
         }
     }
 }
